0fdde14bd800dbeb3413aec1845866ff303a4ab2
Misbah Syed <syedmisbah.ms@gmail.com>
2025-03-20 01:45:27 -0700

    Update office hour UI and controller, added more controllers

---
+++ src/main/java/s25/cs151/application/Controllers/OfficeHours/CoursesContoller.java
@@ -0,0 +1,38 @@
+package s25.cs151.application.Controllers.OfficeHours;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controller for the Office Hours Courses Page
+ * Handles user input for Course information
+ */
+
+public class CoursesContoller implements Initializable{
+
+    /*
+    public TextField courseSection;
+    public TextField courseCode;
+    public TextField courseName;
+    String section = courseSection.getText();
+    String name = courseName.getText();
+     */
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
